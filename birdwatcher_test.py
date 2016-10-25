673c8af5c02481b5bd5eaee36b0d9d21eef920f8
Ed Schouten <ed@kumina.nl>
2016-10-25 11:28:28 +0200

    Also add testing coverage for those hold/keep-alive timers.

--- birdwatcher_test.py
+++ birdwatcher_test.py
@@ -88,6 +88,57 @@ Mesh_10_101_4_114 BGP      master   start  2016-10-23  Active        Socket: Hos
              ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_114",bird_direction="export",bird_action="withdraws",bird_outcome="received"}', 0),
              ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_114",bird_direction="export",bird_action="withdraws",bird_outcome="accepted"}', 0)])
 
+    def test_mesh_up(self):
+        """Tests the metrics output for a mesh route that is up."""
+        text = '''BIRD 1.5.0 ready.
+Mesh_10_101_4_182 BGP      master   up     2016-10-23  Established   
+  Description:    Connection to BGP peer
+  Preference:     100
+  Input filter:   ACCEPT
+  Output filter:  calico_pools
+  Routes:         1 imported, 1 exported, 1 preferred
+  Route change stats:     received   rejected   filtered    ignored   accepted
+    Import updates:              1          0          0          0          1
+    Import withdraws:            0          0        ---          0          0
+    Export updates:             44         25         18        ---          1
+    Export withdraws:            0        ---        ---        ---          0
+  BGP state:          Established
+    Neighbor address: 10.101.4.182
+    Neighbor AS:      64511
+    Neighbor ID:      10.101.4.182
+    Neighbor caps:    refresh enhanced-refresh restart-able AS4 add-path-rx add-path-tx
+    Session:          internal multihop AS4 add-path-rx add-path-tx
+    Source address:   10.101.4.148
+    Hold timer:       177/240
+    Keepalive timer:  61/80
+'''
+        self.assertEqual(
+            list(birdwatcher.parse_show_protocols(StringIO.StringIO(text))),
+            [('bird_up{bird_protocol_instance="Mesh_10_101_4_182"}', 1),
+             ('bird_preference{bird_protocol_instance="Mesh_10_101_4_182"}', 100),
+             ('bird_routes{bird_protocol_instance="Mesh_10_101_4_182",bird_route_type="imported"}', 1),
+             ('bird_routes{bird_protocol_instance="Mesh_10_101_4_182",bird_route_type="exported"}', 1),
+             ('bird_routes{bird_protocol_instance="Mesh_10_101_4_182",bird_route_type="preferred"}', 1),
+             ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_182",bird_direction="import",bird_action="updates",bird_outcome="received"}', 1),
+             ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_182",bird_direction="import",bird_action="updates",bird_outcome="rejected"}', 0),
+             ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_182",bird_direction="import",bird_action="updates",bird_outcome="filtered"}', 0),
+             ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_182",bird_direction="import",bird_action="updates",bird_outcome="ignored"}', 0),
+             ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_182",bird_direction="import",bird_action="updates",bird_outcome="accepted"}', 1),
+             ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_182",bird_direction="import",bird_action="withdraws",bird_outcome="received"}', 0),
+             ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_182",bird_direction="import",bird_action="withdraws",bird_outcome="rejected"}', 0),
+             ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_182",bird_direction="import",bird_action="withdraws",bird_outcome="ignored"}', 0),
+             ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_182",bird_direction="import",bird_action="withdraws",bird_outcome="accepted"}', 0),
+             ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_182",bird_direction="export",bird_action="updates",bird_outcome="received"}', 44),
+             ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_182",bird_direction="export",bird_action="updates",bird_outcome="rejected"}', 25),
+             ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_182",bird_direction="export",bird_action="updates",bird_outcome="filtered"}', 18),
+             ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_182",bird_direction="export",bird_action="updates",bird_outcome="accepted"}', 1),
+             ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_182",bird_direction="export",bird_action="withdraws",bird_outcome="received"}', 0),
+             ('bird_route_changes{bird_protocol_instance="Mesh_10_101_4_182",bird_direction="export",bird_action="withdraws",bird_outcome="accepted"}', 0),
+             ('bird_hold_timer_current{bird_protocol_instance="Mesh_10_101_4_182"}', 177),
+             ('bird_hold_timer_initial{bird_protocol_instance="Mesh_10_101_4_182"}', 240),
+             ('bird_keepalive_timer_current{bird_protocol_instance="Mesh_10_101_4_182"}', 61),
+             ('bird_keepalive_timer_initial{bird_protocol_instance="Mesh_10_101_4_182"}', 80)])
+
 
 if __name__ == '__main__':
     unittest.main()
